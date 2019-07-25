2eb517de46a78ec3ce23dd000bad375013a9f1d5
Zachery Truman <zachery.truman@webteks.com>
2019-07-25 10:03:50 -0400

    Initial import

---
+++ src/main/java/testing/previewmy/net/testing/DemoApplication.java
@@ -0,0 +1,13 @@
+package testing.previewmy.net.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
